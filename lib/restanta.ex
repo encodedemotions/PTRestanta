98578fdfc1db5e83a1b7d022459be3b2c4a86cea
encodedemotions <57257684+encodedemotions@users.noreply.github.com>
2022-11-03 15:35:06 +0200

    Add project

---
+++ lib/restanta.ex
@@ -0,0 +1,18 @@
+defmodule Restanta do
+  @moduledoc """
+  Documentation for `Restanta`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Restanta.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/restanta_test.exs
@@ -0,0 +1,8 @@
+defmodule RestantaTest do
+  use ExUnit.Case
+  doctest Restanta
+
+  test "greets the world" do
+    assert Restanta.hello() == :world
+  end
+end
